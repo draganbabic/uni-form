c54c687f42f6277c916c6392a4e94bc9c4a765a2
Dragan Babic <draganbabic@draganbabic.(none)>
2010-07-09 16:20:25 +0200

    Adding an improved version of the jQuery plugin with support for coloring and removing existing text input's value, by Toni Karlheinz

--- js/uni-form.jquery.js
+++ js/uni-form.jquery.js
@@ -6,22 +6,37 @@ jQuery.fn.uniform = function(settings) {
     valid_class    : 'valid',
     invalid_class  : 'invalid',
     focused_class  : 'focused',
-    holder_class   : 'ctrlHolder',
-    field_selector : 'input, select, textarea'
+    holder_class   : 'element',
+    field_selector : ':text, textarea',
+    default_value_color: "#AFAFAF"
   }, settings);
   
   return this.each(function() {
     var form = jQuery(this);
-    // Select form fields and attach them higlighter functionality
-    form.find(settings.field_selector).focus(function() {
-      form.find('.' + settings.focused_class).removeClass(settings.focused_class);
-      $(this).parents().filter('.'+settings.holder_class+':first').addClass(settings.focused_class);
-    }).blur(function() {
-      form.find('.' + settings.focused_class).removeClass(settings.focused_class);
+    form.submit(function(){
+      form.find(settings.field_selector).each(function(){
+        if($(this).val() == $(this).attr("default_value")) $(this).val("");
+      });
     });
+    // Select form fields and attach them higlighter functionality
+    form.find(settings.field_selector).each(function(){
+      var default_value = $(this).val();
+      var default_color = $(this).css("color");
+      if($(this).val() == default_value){$(this).css("color",settings.default_value_color);};
+      $(this).attr("default_value", default_value);
+      $(this).focus(function() {
+       form.find('.' + settings.focused_class).removeClass(settings.focused_class);
+        $(this).parents().filter('.'+settings.holder_class+':first').addClass(settings.focused_class);
+        if($(this).val() == default_value){ $(this).val("");$(this).css("color",default_color); };
+      }).blur(function() {
+        form.find('.' + settings.focused_class).removeClass(settings.focused_class);
+        if($(this).val() == ""){$(this).css("color",settings.default_value_color);$(this).val(default_value);};
+      });
+    })
   });
 };
 // Auto set on page load...
 $(document).ready(function() {
-  jQuery('form.uniForm').uniform();
+  jQuery('form').uniform();
 });
+
