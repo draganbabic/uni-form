92e928a9fe0d3a93d257f593486d29b12ad18aac
Umut Fikret GÜRKAVCU <umut.gurkavcu@4primes.com>
2011-06-18 02:00:31 +0300

    Turkish localization added

---
+++ localization/tr.js
@@ -0,0 +1,27 @@
+// Turkish localization by umut Fikret Gürkavcu
+// umut.gurkavcu@4primes.com
+
+jQuery.fn.uniform.language = jQuery.extend(jQuery.fn.uniform.language, {
+    required      : '%s alanının girilmesi zorunludur',
+    req_radio     : 'Lütfen bir seçim yapınız',
+    req_checkbox  : 'Devam etmek için bu kutuyu işaretlemiş olmanız gerekiyor',
+    minlength     : '%s alanına en az %d karakter girilmiş olmalı',
+    min           : '%s alanı %d karakter veya daha fazla karakter içermelidir',
+    maxlength     : '%s alanı %d karakterden daha fazla olamaz',
+    max           : '%s alanı %d karakter veya daha az karakter içermelidir',
+    same_as       : '%s alanının %s alanıyla aynı olması gerekiyor',
+    email         : 'Geçerli bir e-posta adresi değil',
+    url           : '%s geçerli bir adres değil',
+    number        : '%s alanı sayı olmalı',
+    integer       : '%s alanı tam sayı olmalı',
+    alpha         : '%s alanı sadece harflerden oluşabilir',
+    alphanum      : '%s alanı harflerden ve sayılardan oluşabilir',
+    phrase        : '%s alanı harflerden , sayılardan , boşluktan veya şu karakterlerden . , - _ () * # : oluşabilir',
+    phone         : '%s alanı geçerli bir telefon numarası değil',
+    date          : '%s alanı geçerli bir tarih olmalıdır (gün/ay/yıl)',
+    callback      : '%s alanının doğrulaması yapılamadı. Doğrulama fonksiyonu (%s) belirtilmemiş!',
+    on_leave      : 'Bu sayfadaki bilgileri kaydetmeden ayrılmak istediğinize emin misiniz?',
+    submit_msg    : 'Sayfada düzeltilmesi gereken alanlar bulunuyor',
+    submit_help   : 'Lütfen aşağıdaki maddeleri gözden geçiriniz.',
+    submit_success: 'Teşekkürler bilgiler gönderildi.'
+});
